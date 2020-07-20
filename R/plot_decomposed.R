630f755fe62895b0168c3c26d322225f36de5680
Ursula Laa <uschi.laa@gmx.at>
2020-07-21 08:43:50 +1000

    Notes fixed for plot_decomposed.

--- R/plot_decomposed.R
+++ R/plot_decomposed.R
@@ -1,9 +1,12 @@
 #' Plot decomposed time series from comp_tsout_ens or mv_tsout_ens output.
 #'
 #' @param obj The output from \code{comp_tsout_ens} or \code{mv_tsout_ens} functions.
-#' @param X The data matrix used as input to \code{comp_tsout_ens} or \code{mv_tsout_ens}.
+#' @param X The data matrix used as input to \code{mv_tsout_ens} (not needed if \code{obj} is output from \code{comp_tsout_ens}).
 #' @param method The decomposition method, choose between "pca" (default), "dobin", "ics" or "ica".
 #' @return A ggplot showing the time series from the selected decomposition method.
+#'
+#' @importFrom dplyr %>%
+#' @importFrom rlang .data
 #'
 #' @examples
 #' set.seed(100)
@@ -15,25 +18,42 @@
 #' x3 <- sample(1:100, n, replace=TRUE)
 #' x4 <- sample(1:100, n, replace=TRUE)
 #' X <- cbind.data.frame(x, x2, x3, x4)
-#' out <- mv_tsout_ens(X, compr=2, fast=FALSE)
-#' plot_decomposed(out, X, method = "pca")
+#'
+#' out1 <- mv_tsout_ens(X, compr=2, fast=FALSE)
+#' plot_decomposed(out1, X = X, method = "pca")
+#'
+#' X <- X/rowSums(X)
+#' out2 <- comp_tsout_ens(X, compr=2, fast=FALSE)
+#' plot_decomposed(out2)
 #'
 #' @export
-plot_decomposed <- function(obj, X, method = "pca"){
+plot_decomposed <- function(obj, X = NULL, method = "pca"){
+  # initial check that loadings for requested method are in obj
   loading_mat <- obj[[paste0(method, "_loadings")]]
   if(! is.matrix(loading_mat)){
     print(paste0("Error: loadings for method ", method, " not found!"))
     return(NULL)
   }
+  # for comp_tsout_ens we can get data from obj$comp_coords
+  # otherwise data needs to be passe in as matrix X
+  if(is.null(obj$comp_coords)){
+    if(is.null(X)){
+      print(paste0("Error: need data matrix X to plot mv_tsout_ens output."))
+      return(NULL)
+    }
+  }
+  else{
+    X <- obj$comp_coords
+  }
   ts_proj <- as.matrix(X) %*% loading_mat
   colnames(ts_proj) <- paste0(method, "_", 1:ncol(loading_mat))
   ts_proj <- tibble::as_tibble(ts_proj) %>%
     dplyr::mutate(t = 1:nrow(X)) %>%
-    tidyr::pivot_longer(-t)
+    tidyr::pivot_longer(-.data$t, values_to = "value")
 
-  ggplot2::ggplot(ts_proj, ggplot2::aes(x=t, y=value)) +
+  ggplot2::ggplot(ts_proj, ggplot2::aes(x=.data$t, y=.data$value)) +
     ggplot2::geom_line() +
     ggplot2::geom_vline(xintercept = obj$outliers[,"Indices"], color="red", alpha=0.8, size=0.1) +
-    ggplot2::facet_wrap(~name, ncol = 2) +
+    ggplot2::facet_wrap(~.data$name, ncol = 2) +
     ggplot2::theme_bw()
 }
