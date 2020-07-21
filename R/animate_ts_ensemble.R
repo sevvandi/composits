240b2a17a5c35c77ff3e9ee810e2ca89cbcff07c
Ursula Laa <uschi.laa@gmx.at>
2020-07-21 15:15:40 +1000

    Allowing tour on dim red spaces.

--- R/animate_ts_ensemble.R
+++ R/animate_ts_ensemble.R
@@ -2,6 +2,7 @@
 #'
 #' @param obj The output from \code{comp_tsout_ens} or \code{mv_tsout_ens} functions.
 #' @param X The data matrix used as input to \code{mv_tsout_ens} (not needed if \code{obj} is output from \code{comp_tsout_ens}).
+#' @param method The dimension reduction method to apply before running the tour (if \code{NULL} tour on the full data space).
 #' @param max_frames The maximum number of bases to generate in the grand tour (default is Inf).
 #'
 #' @examples \donttest{
@@ -21,12 +22,12 @@
 #' animate_ts_ensemble(out1, X, max_frames = 10)
 #'
 #' X <- X/rowSums(X)
-#' out2 <- comp_tsout_ens(X, compr=2, fast=FALSE)
-#' animate_ts_ensemble(out2, max_frames = 10)
+#' out2 <- comp_tsout_ens(X, ncomp = 3, compr=2, fast=FALSE)
+#' animate_ts_ensemble(out2, method = "dobin", max_frames = 10)
 #' }
 #'
 #' @export
-animate_ts_ensemble <- function(obj=NULL, X = NULL, max_frames = Inf){
+animate_ts_ensemble <- function(obj=NULL, X = NULL, method = NULL, max_frames = Inf){
   # check that we have the right input
   if (is.null(X)){
     if (is.null(obj$comp_coords)){
@@ -35,8 +36,25 @@ animate_ts_ensemble <- function(obj=NULL, X = NULL, max_frames = Inf){
     }
     X <- obj$comp_coords
   }
+  if (!is.null(method)){
+    loading_mat <- obj[[paste0(method, "_loadings")]]
+    if(! is.matrix(loading_mat)){
+      print(paste0("Warning: loadings for method ", method, " not found! Using original data."))
+    }
+    else{
+      X <- as.matrix(X) %*% loading_mat
+    }
+  }
+  if(ncol(X) < 3){
+    print(paste0("Error: need at least 3 coordinates for running a tour!"))
+    return(NULL)
+  }
+  if(is.null(colnames(X))){
+    coord_name <- ifelse(is.null(method), "x", method)
+    colnames(X) <- paste0(coord_name, 1:ncol(X))
+  }
   # color for tourr highlights outliers
   col <- rep("black", nrow(X))
   col[obj$outliers[,"Indices"]] <- "red"
-  tourr::animate_xy(X, col=col, axes="off", max_frames = max_frames)
+  tourr::animate_xy(X, col=col, axes="bottomleft", max_frames = max_frames)
 }
